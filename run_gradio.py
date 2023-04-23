5c6afafccbc3af31e8e6f76736151c9050184b54
Alexander Sokol <sokol.a@abcloudz.com>
2023-04-23 13:47:41 +0300

    add javascript loader for run_gradio.py

--- run_gradio.py
+++ run_gradio.py
@@ -1,12 +1,72 @@
+import mimetypes
+import os
 import time
 
 import gradio as gr
+import gradio.routes
 
 from scripts.mo.environment import *
 from scripts.mo.init_storage import initialize_storage
 from scripts.mo.ui_main import main_ui_block
 
 SETTINGS_FILE = 'settings.txt'
+
+mimetypes.init()
+mimetypes.add_type("application/javascript", ".js")
+
+
+class ScriptLoader:
+    path_map = {
+        "js": os.path.abspath(os.path.join(os.path.dirname(__file__), "javascript")),
+    }
+
+    def __init__(self, script_type):
+        self.script_type = script_type
+        self.path = ScriptLoader.path_map[script_type]
+        self.loaded_scripts = []
+
+    @staticmethod
+    def get_scripts(path: str, file_type: str) -> list[tuple[str, str]]:
+        """Returns list of tuples
+        Each tuple contains the full filepath and filename as strings
+        """
+        scripts = []
+        dir_list = [os.path.join(path, f) for f in os.listdir(path)]
+        files_list = [f for f in dir_list if os.path.isfile(f)]
+        for s in files_list:
+            # Don't forget the "." for file extension
+            if os.path.splitext(s)[1] == f".{file_type}":
+                scripts.append((s, os.path.basename(s)))
+        return scripts
+
+
+class JavaScriptLoader(ScriptLoader):
+    def __init__(self):
+        # Script type set here
+        super().__init__("js")
+        # Copy the template response
+        self.original_template = gradio.routes.templates.TemplateResponse
+        # Prep the js files
+        self.load_js()
+        # reassign the template response to your method, so gradio calls your method instead
+        gradio.routes.templates.TemplateResponse = self.template_response
+
+    def load_js(self):
+        js_scripts = ScriptLoader.get_scripts(self.path, self.script_type)
+        for file_path, file_name in js_scripts:
+            with open(file_path, 'r', encoding="utf-8") as file:
+                self.loaded_scripts.append(f"\n<!--{file_name}-->\n<script>\n{file.read()}\n</script>")
+
+    def template_response(self, *args, **kwargs):
+        """Once gradio calls your method, you call the original, you modify it to include
+        your scripts and you return the modified version
+        """
+        response = self.original_template(*args, **kwargs)
+        response.body = response.body.replace(
+            '</head>'.encode('utf-8'), f"{''.join(self.loaded_scripts)}\n</head>".encode("utf-8")
+        )
+        response.init_headers()
+        return response
 
 
 def read_settings():
@@ -162,5 +222,6 @@ with gr.Blocks() as demo:
     with gr.Tab("Testing"):
         testing_block()
 
+js_loader = JavaScriptLoader()
 demo.queue()
 demo.launch()
