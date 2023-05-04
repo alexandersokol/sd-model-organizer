fe43d86a300f73f9961f3cf42cc386f3bba8a4ba
Alexander Sokol <sokol.a@abcloudz.com>
2023-05-04 11:45:12 +0300

    fix firebase-admin dependency installation

--- install.py
+++ install.py
@@ -4,4 +4,4 @@ if not launch.is_installed("bs4"):
     launch.run_pip("install bs4", "Required to parse GDisk links")
 
 if not launch.is_installed("firebase-admin"):
-    launch.run_pip("firebase-admin", "Required for Firebase Storage")
+    launch.run_pip("install firebase-admin", "Required for Firebase Storage")
