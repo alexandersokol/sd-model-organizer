e1418fe8800e789faafa893b6eee826f561aaf4c
Alexander Sokol <sokol.a@abcloudz.com>
2023-05-04 14:30:56 +0300

    add firebase-admin package version

--- install.py
+++ install.py
@@ -4,4 +4,4 @@ if not launch.is_installed("bs4"):
     launch.run_pip("install bs4", "bs4 requirement for GDick links parsing")
 
 if not launch.is_installed("firebase-admin"):
-    launch.run_pip("install firebase-admin", "firebase-admin requirement for the Firestore usage")
+    launch.run_pip("install firebase-admin==4.5.0", "firebase-admin requirement for the Firestore usage")
