968b92c861c44cb3918781bd510aca55349c6375
Oleksandr Sokol <sokol.kh@gmail.com>
2023-06-21 20:09:49 +0300

    fix preview download error

--- scripts/mo/dl/download_manager.py
+++ scripts/mo/dl/download_manager.py
@@ -280,8 +280,9 @@ class DownloadManager:
 
                 preview_downloader = self._get_downloader(record.preview_url)
 
-                with tempfile.NamedTemporaryFile(dir=destination_dir) as temp:
+                with tempfile.NamedTemporaryFile(dir=destination_dir, delete=False) as temp:
                     logger.debug('Downloading preview into tmp file: %s', temp.name)
+                    self._temp_files.add(temp)
                     for upd in preview_downloader.download(record.preview_url, temp.name, preview_filename,
                                                            self._stop_event):
                         yield {'preview_dl': upd}
